88e0d234b74c2b478c8d925580433a4541fdb4be
Pietro <74447932+pietroepis@users.noreply.github.com>
2020-12-23 12:20:04 +0100

    Regionprops
    Classificatore forme

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ close all;
 
 load("classifier_bayes.mat");
 
-% TEST 
+% SCENA
 image = imread("P10.jpg");
 imrgb= imresize(image, 0.4);
 imycbcr = rgb2ycbcr(imrgb);
@@ -22,3 +22,24 @@ predicted = medfilt2(predicted, [19 19]);
 se = strel("square", 30);
 predicted = imerode(predicted, se);
 figure, imshow(predicted);
+
+props = regionprops(logical(bwlabel(predicted)), "Centroid", "Eccentricity", "Circularity");
+scene_props = [];
+for i=1:length(props)
+    scene_props = [scene_props; props(i).Centroid props(i).Eccentricity props(i).Circularity];
+end
+
+% SCHEMA
+imscheme = rgb2gray(im2double(imread("S01.jpg")));
+mask = imscheme > 0.39;
+mask = medfilt2(mask, [7 7]);
+labels = bwlabel(mask);
+props = regionprops(logical(labels), "Centroid", "Eccentricity", "Circularity");
+scheme_props = [];
+for i=1:length(props)
+    scheme_props = [scheme_props; props(i).Centroid props(i).Eccentricity props(i).Circularity];
+end
+
+% CLUSTERIZATION
+is_scheme = [zeros(length(scene_props), 1); ones(length(scheme_props), 1)];
+all_props = [scene_props; scheme_props];
