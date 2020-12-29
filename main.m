fcf669ff2cd1d5c3e86065e3aaba383233c24f41
Blowdire <agazzi.ruben99@gmail.com>
2020-12-29 12:01:02 +0100

    riconoscimento forme schema

--- main.m
+++ main.m
@@ -10,17 +10,6 @@ imrgb= imresize(image, 1);
 imycbcr = rgb2ycbcr(imrgb);
 imhsv = rgb2hsv(imrgb);
 
-% figure
-% subplot(3, 3, 1), imshow(imrgb(:,:,1));
-% subplot(3, 3, 2), imshow(imrgb(:,:,2));
-% subplot(3, 3, 3), imshow(imrgb(:,:,3));
-% subplot(3, 3, 4), imshow(imycbcr(:,:,1));
-% subplot(3, 3, 5), imshow(imycbcr(:,:,2));
-% subplot(3, 3, 6), imshow(imycbcr(:,:,3));
-% subplot(3, 3, 7), imshow(imhsv(:,:,1));
-% subplot(3, 3, 8), imshow(imhsv(:,:,2));
-% subplot(3, 3, 9), imshow(imhsv(:,:,3));
-
 test_values = cat(3, imrgb(:, :, 2:3), imycbcr);
 % test_values = cat(3, imycbcr(:,:,2:3), imhsv(:,:,2));
 [r, c, ch] = size(test_values);
@@ -45,7 +34,7 @@ labeled_scene = bwlabel(predicted);
 scene_labels = unique(labeled_scene);
 scene_props = [];
 for i = 2:length(scene_labels)
-    im_props = regionprops(labeled_scene == scene_labels(i), "Circularity", "Solidity", "Eccentricity", "BoundingBox", "Area", "Perimeter");
+    im_props = regionprops(labeled_scene == scene_labels(i), "BoundingBox");
     subImage = imcrop(labeled_scene== scene_labels(i),im_props.BoundingBox);
     subImage = padarray(subImage, [100 100], 0 , 'both');
   
@@ -63,8 +52,8 @@ for i = 2:length(scene_labels)
     
     %-----------------------------------------
     corners = detectHarrisFeatures(simplified, "MinQuality", 0.35, "FilterSize", 11);
-    im_props = regionprops(subImage, "All");
-    scene_props = [scene_props;  corners.Count/8 im_props.Eccentricity  im_props.Area/im_props.Perimeter^2 scene_labels(i)]
+    im_props = regionprops(subImage, "Eccentricity", "Area", "Perimeter");
+    scene_props = [scene_props;  corners.Count/8 im_props.Eccentricity  im_props.Area/im_props.Perimeter^2 scene_labels(i)];
     
 end
 
@@ -77,22 +66,38 @@ scheme_labels = unique(labeled_scheme);
 scheme_props = [];
 
 for i = 3:length(scheme_labels)
-    props = regionprops(labeled_scheme == scheme_labels(i), "Circularity", "Extent", "Eccentricity", "Perimeter","BoundingBox");
-    
-    scheme_props = [scheme_props; props.Circularity props.Eccentricity props.Extent props.Perimeter scheme_labels(i)];
-end
+    im_props = regionprops(labeled_scheme == scheme_labels(i),"BoundingBox");
+    subImage = imcrop(labeled_scheme == scheme_labels(i),im_props.BoundingBox);
+    subImage = padarray(subImage, [100 100], 0 , 'both');
+  
+    %subImage = imresize(subImage, 0.3, 'nearest');
+   %poly reduction----------------------
+    [B, L] = bwboundaries(subImage, 'noholes');
+     boundary = B{1};
 
-% CLUSTERIZATION
-is_scheme = [ones(length(scheme_props), 1); zeros(length(scene_props), 1);];
-%all_props = [scheme_props; scene_props];
+    tolerance = 0.08;
+    %Ramer-Douglas-Peucker algorithm
+    p_reduced = reducepoly(boundary,tolerance);    
+    [X, Y] = size(subImage);
+    simplified = zeros(X, Y);
+    simplified = roipoly(simplified, p_reduced(:,2),p_reduced(:,1));
+    
+    %-----------------------------------------
+    corners = detectHarrisFeatures(simplified, "MinQuality", 0.35, "FilterSize", 11);
+    im_props = regionprops(subImage,  "Eccentricity", "Area", "Perimeter");
+    scheme_props = [scheme_props;  corners.Count/8 im_props.Eccentricity  im_props.Area/im_props.Perimeter^2 scene_labels(i)];
+end
 
 for i=1:length(scene_props)
-    props = scene_props(i,:)
+    props = scene_props(i,:);
     label=predict(classifier_knn,props(1:end-1));
     figure, imshow(labeled_scene==props(end)), title(label);
+end
+
+for i=1:length(scheme_props)
+      props = scheme_props(i,:);
+      label=predict(classifier_knn,props(1:end-1));
+      figure, imshow(labeled_scheme==props(end)), title(label);
 
 end
 
-% n_clusters = 9;% max(max(length(scheme_labels) - 2, scene_labels));
-% idx = kmeans(all_props(:,1:end-1), n_clusters);
-% final = [all_props(:,1:end-1) is_scheme idx all_props(:,end)];
