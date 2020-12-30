09328c8db924219eee195da1650802b872199854
Pietro <pietroepis@gmail.com>
2020-12-30 10:45:27 +0100

    Resize

--- test2.m
+++
@@ -1,9 +0,0 @@
-[x, y,ch] = size(imrgb);
-load('bayesian_model.mat');
-test_predicted=predict(classifier_bayes,test_values);
-
-%show_result(image,mask_predicted);
-
-
-
-cm = confmat(test_labels,test_predicted);
